0d21b14e901dd1e8cdb648e040bee759a2c613cb
armpit <armpit@rumpigs.net>
2016-09-15 20:51:07 +1000

    Move progress bar into a function and add shutdown option.

--- retropie/as_menu.sh
+++ retropie/as_menu.sh
@@ -5,8 +5,20 @@ _menu=true
 DIALOG_CANCEL=1
 DIALOG_ESC=255
 steam_cmd="/usr/local/bin/steam"
-es_cmd=""
+es_cmd="/usr/bin/emulationstation"
 
+function pbar()
+{
+	{
+		for ((i = 0 ; i <= 100 ; i+=5));
+		do
+			sleep 0.1
+			echo $i
+		done
+	} | whiptail --gauge "${_msg}" 6 50 0
+}
+
+# Menu Loop
 while $_menu;
 do
 	exec 3>&1
@@ -14,10 +26,11 @@ do
 	selection=$(whiptail --title "Interface Select" \
 		--backtitle "into the wonderfull" \
 		--clear \
-		--menu "Select" 20 78 3 \
+		--menu "Select" 20 78 4 \
 		"1" "Emulation Station" \
 		"2" "Steam" \
 		"3" "Shell" \
+		"4" "Shutdown System" \
 		2>&1 1>&3)
 
 	exit_status=$?
@@ -38,29 +51,26 @@ do
 	1)
 		_menu=false
 		clear
-		{
-		for ((i = 0 ; i <= 100 ; i+=5)); do
-			sleep 0.1
-			echo $i
-		done
-		} | whiptail --gauge "Loading Emulation Station..." 6 50 0
-		
+		_msg="Starting Emulation Station..."
+		pbar
+		exec $es_cmd
 		;;
 	2)
 		_menu=false
 		clear
-		{
-		for ((i = 0 ; i <= 100 ; i+=5));
-		do
-			sleep 0.1
-			echo $i
-		done
-		} | whiptail --gauge "Booting into steam..." 6 50 0
+		_msg="Connecting to Steam..."
+		pbar
 		exec $steam_cmd
 		;;
 	3)
 		_menu=false
 		clear
+		;;
+	4)
+		_menu=false
+		_msg="Shutting down system..."
+		pbar
+		sudo halt
 		;;
 	esac
 
