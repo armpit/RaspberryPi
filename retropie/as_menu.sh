2b03cecfe39719c4bed1a965bb5ffbafcf75e166
armpit <armpit@rumpigs.net>
2016-09-18 23:05:08 +1000

    Add a shutdown option and option to launch mehstation.

--- retropie/as_menu.sh
+++ retropie/as_menu.sh
@@ -6,6 +6,7 @@ DIALOG_CANCEL=1
 DIALOG_ESC=255
 steam_cmd="/usr/local/bin/steam"
 es_cmd="/usr/bin/emulationstation"
+ms_cmd="LD_LIBRARY_PATH=/usr/local/lib mehstation"
 
 function pbar()
 {
@@ -26,11 +27,13 @@ do
 	selection=$(whiptail --title "Interface Select" \
 		--backtitle "into the wonderfull" \
 		--clear \
-		--menu "Select" 20 78 4 \
-		"1" "Emulation Station" \
-		"2" "Steam" \
-		"3" "Shell" \
-		"4" "Shutdown System" \
+		--menu "Select" 20 78 6 \
+		"1" "Desktop" \
+		"2" "Emulation Station" \
+		"3" "mehstation" \
+		"4" "Steam" \
+		"5" "Shell" \
+		"6" "Shutdown System" \
 		2>&1 1>&3)
 
 	exit_status=$?
@@ -51,22 +54,36 @@ do
 	1)
 		_menu=false
 		clear
+		_msg="Starting desktop..."
+		pbar
+		exec $dt_cmd
+		;;
+	2)
+		_menu=false
+		clear
 		_msg="Starting Emulation Station..."
 		pbar
 		exec $es_cmd
 		;;
-	2)
+	3)
+		_menu=false
+		clear
+		_msg="Starting mehstation..."
+		pbar
+		exec $ms_cmd
+		;;
+	4)
 		_menu=false
 		clear
 		_msg="Connecting to Steam..."
 		pbar
 		exec $steam_cmd
 		;;
-	3)
+	5)
 		_menu=false
 		clear
 		;;
-	4)
+	6)
 		_menu=false
 		_msg="Shutting down system..."
 		pbar
