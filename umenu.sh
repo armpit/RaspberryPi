969cf132364bcc506c0a8b6d014a2fba37a32879
armpit <armpit@rumpigs.net>
2016-10-09 18:20:33 +1000

    Add dosbox and reboot.

--- umenu.sh
+++ umenu.sh
@@ -8,6 +8,7 @@ steam_cmd="/usr/local/bin/steam"
 dt_cmd="startx"
 es_cmd="/usr/bin/emulationstation"
 ms_cmd="/usr/bin/mehstation"
+db_cmd="/usr/bin/dosbox"
 
 function pbar()
 {
@@ -28,13 +29,15 @@ do
 	selection=$(whiptail --title "Interface Select" \
 		--backtitle "into the wonderfull" \
 		--clear \
-		--menu "Select" 20 78 6 \
+		--menu "Select" 20 78 8 \
 		"1" "Desktop" \
-		"2" "Emulation Station" \
-		"3" "mehstation" \
-		"4" "Steam" \
-		"5" "Shell" \
-		"6" "Shutdown System" \
+		"2" "Dosbox" \
+		"3" "Emulation Station" \
+		"4" "mehstation" \
+		"5" "Steam" \
+		"6" "Shell" \
+		"7" "Reboot" \
+		"8" "Shutdown System" \
 		2>&1 1>&3)
 
 	exit_status=$?
@@ -62,11 +65,18 @@ do
 	2)
 		_menu=false
 		clear
+		_msg="Launching Dosbox..."
+		pbar
+		exec $db_cmd
+		;;
+	3)
+		_menu=false
+		clear
 		_msg="Starting Emulation Station..."
 		pbar
 		exec $es_cmd
 		;;
-	3)
+	4)
 		_menu=false
 		clear
 		_msg="Starting mehstation..."
@@ -74,18 +84,24 @@ do
 		LD_LIBRARY_PATH=/usr/local/lib
 		exec $ms_cmd
 		;;
-	4)
+	5)
 		_menu=false
 		clear
 		_msg="Connecting to Steam..."
 		pbar
 		exec $steam_cmd
 		;;
-	5)
+	6)
 		_menu=false
 		clear
 		;;
-	6)
+	7)
+		_menu=false
+		_msg="Rebooting system..."
+		pbar
+		sudo reboot
+		;;
+	8)
 		_menu=false
 		_msg="Shutting down system..."
 		pbar
